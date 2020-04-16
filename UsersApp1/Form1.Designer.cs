420249ffa1b788cbbbcf835d3544d33ecc63bf1a
Олександр Гнідін <yozhegsan@gmail.com>
2020-04-16 15:35:03 +0300

    Добавьте файлы проекта.

---
+++ UsersApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace UsersApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.Location = new System.Drawing.Point(356, 212);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(75, 18);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "0";
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblDone
+            // 
+            this.lblDone.AutoSize = true;
+            this.lblDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDone.Location = new System.Drawing.Point(261, 72);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(277, 108);
+            this.lblDone.TabIndex = 1;
+            this.lblDone.Text = "Done";
+            this.lblDone.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblDone);
+            this.Controls.Add(this.lbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lblDone;
+    }
+}
+
---
+++ UsersApp1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.Protocols;
+using System.Collections;
+using System.DirectoryServices.AccountManagement;
+
+namespace UsersApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Log(string strLOG)
+        {
+            if (strLOG.Length == 0) return;
+            string[] lines = { strLOG };
+            System.IO.File.AppendAllLines(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserApp1LOG.txt"), lines, System.Text.Encoding.Default);
+        }
+
+        private void Test1()
+        {
+            string st = "";
+            int n = 0;
+            try
+            {
+                using (var context = new PrincipalContext(ContextType.Domain, "mhp.com.ua.inc"))
+                {
+                    using (var searcher = new PrincipalSearcher(new UserPrincipal(context)))
+                    {
+                        foreach (var result in searcher.FindAll())
+                        {
+                            DirectoryEntry de = result.GetUnderlyingObject() as DirectoryEntry;
+                            n++;
+                            st = n.ToString("00000") + " Користувач: " + de.Properties["sn"].Value + " " + de.Properties["givenName"].Value + "(" + de.Properties["samAccountName"].Value + ") " + de.Properties["userPrincipalName"].Value;
+                            lbl.Text = n.ToString(); ;
+                            Log(st);
+                            Application.DoEvents();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            lblDone.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            lbl.Text = "0";
+            lblDone.Visible = false;
+            Test1();
+            button1.Enabled = true;
+        }
+    }
+}
